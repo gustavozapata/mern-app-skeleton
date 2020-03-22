350f98b377ab1a4d53ebede3a3bfb4d5e8769537
Gustavo Zapata <tavordie@hotmail.com>
2020-03-22 11:35:37 +0000

    cleaning up

--- server/app.js
+++ server/app.js
@@ -8,7 +8,10 @@ const itemRouter = require("./routes/itemRoutes");
 const app = express();
 
 //MIDDLEWARE
-app.use(morgan("dev")); //logs all the end-point calls
+// if (process.env.NODE_env === "development") { USE THIS ONE WHEN USING dotenv()
+if (app.get("env") === "development") {
+  app.use(morgan("dev")); //logs all the end-point calls
+}
 app.use(cors()); //allows *
 app.use(express.static("public")); //serves files (html, css, js) in the public folder
 app.use(express.json()); //allows us to access the body of the request
